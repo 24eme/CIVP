3215ab21e65c5d1af78a5e85906ff7932469685c
Mame <mngom@24eme.fr>
2021-01-19 17:52:45 +0100

    Structure de la page

---
+++ resources/views/components/_sideNav.blade.php
@@ -0,0 +1,8 @@
+<div id="side_navigation">
+  <div class="filter-wrapper">
+    <div class="filter-div"><label class="filter-label" for="FilterProdRec">Producteur - Récoltant</label></div><label class="switch"><input class="filter-profil" id="FilterProdRec" name="producteur" value="Producteur-Recoltant" type="checkbox" checked><span id="sliderProd" class="slider round"></span></label>
+    <div class="filter-div"><label class="filter-label" for="FilterNegociant">Négociant</label></div><label class="switch"><input class="filter-profil" id="FilterNegociant" name="nego" value="Negociant" type="checkbox" checked><span id="sliderNego" class="slider round"></span></label>
+    <div class="filter-div"><label class="filter-label" for="FilterNegociantVini">Négociant - Vinificateur</label></div><label class="switch"><input class="filter-profil" id="FilterNegociantVini" name="nego" value="Negociant-Vinificateur" type="checkbox" checked><span id="sliderVini" class="slider round"></span></label>
+    <div class="filter-div"><label class="filter-label" for="FilterViticulteur">Viticulteur ou exploitant agricole</label></div><label class="switch"><input class="filter-profil" id="FilterViticulteur" name="viti" value="Viticulteur" type="checkbox" checked><span id="sliderViti" class="slider round"></span></label>
+  </div>
+</div>
