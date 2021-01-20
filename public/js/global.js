205545ee332dbe2f8cc9cfad598489722e5ef477
Mame <mngom@24eme.fr>
2021-01-20 16:46:03 +0100

    Modification CSS Menu :

---
+++ public/js/global.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("side_navigation").style.width = "350px";
+  document.getElementById("main").style.marginLeft = "350px";
+}
+
+function closeNav() {
+  document.getElementById("side_navigation").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
