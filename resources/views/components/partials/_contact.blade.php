6886a4d097c74f2f9a7d1dbb0d8c720ba35b2702
Mame <mngom@24eme.fr>
2021-01-28 11:19:03 +0100

    Ajout fichier pour visualisation des contact

---
+++ resources/views/components/partials/_contact.blade.php
@@ -0,0 +1,57 @@
+<section>
+<svg height="100%" width="110%" id="bg-svg" viewBox="0 0 1440 200" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><defs><linearGradient id="gradient"><stop offset="5%" stop-color="#edc0b788"></stop><stop offset="95%" stop-color="#cf7b7c88"></stop></linearGradient></defs><path d="M 0,600 C 0,600 0,200 0,200 C 84.47846889952157,226 168.95693779904315,252 258,267 C 347.04306220095685,282 440.6507177033492,286 553,270 C 665.3492822966508,254.00000000000003 796.4401913875599,218.00000000000003 910,192 C 1023.5598086124401,165.99999999999997 1119.5885167464114,150 1205,153 C 1290.4114832535886,156 1365.2057416267944,178 1440,200 C 1440,200 1440,600 1440,600 Z" stroke="none" stroke-width="0" fill="url(#gradient)" class="transition-all duration-300 ease-in-out delay-150"></path><defs><linearGradient id="gradient"><stop offset="5%" stop-color="#edc0b7ff"></stop><stop offset="95%" stop-color="#cf7b7cff"></stop></linearGradient></defs><path d="M 0,600 C 0,600 0,400 0,400 C 98.80382775119617,413.70334928229664 197.60765550239233,427.4066985645933 299,422 C 400.39234449760767,416.5933014354067 504.37320574162675,392.0765550239235 588,393 C 671.6267942583733,393.9234449760765 734.8995215311005,420.2870813397129 832,429 C 929.1004784688995,437.7129186602871 1060.0287081339713,428.77511961722485 1167,421 C 1273.9712918660287,413.22488038277515 1356.9856459330144,406.6124401913876 1440,400 C 1440,400 1440,600 1440,600 Z" stroke="none" stroke-width="0" fill="url(#gradient)" class="transition-all duration-300 ease-in-out delay-150"></path></svg>
+<div class="contact-section">
+  <div class="contact-container">
+    <h3>CIVP</h3>
+    <hr>
+    <p>CONSEIL INTERPROFESSIONNEL DES VINS DE PROVENCE</p>
+    <div class="">
+      <p class="address-info">MAISON DES VINS</p>
+      <p class="address-info">RN 7 - 83460 LES ARCS SUR ARGENS</p>
+      <p>Tél : <a href="tel:0634789201">0634789201</a></p>
+      <p>Tél : <a href="fax:0134789201">0134789201</a></p>
+      <p>Mail : <a href="mailto:">vins@provence.fr</a></p>
+    </div>
+  </div>
+  <div class="contact-container">
+    <h3>CIVP</h3>
+    <hr>
+    <p>CONSEIL INTERPROFESSIONNEL DES VINS DE PROVENCE</p>
+    <div class="">
+      <p class="address-info">MAISON DES VINS</p>
+      <p class="address-info">RN 7 - 83460 LES ARCS SUR ARGENS</p>
+      <p>Tél : <a href="tel:0634789201">0634789201</a></p>
+      <p>Tél : <a href="fax:0134789201">0134789201</a></p>
+      <p>Mail : <a href="mailto:">vins@provence.fr</a></p>
+    </div>
+  </div>
+  <div class="contact-container">
+    <h3>CIVP</h3>
+    <hr>
+    <p>CONSEIL INTERPROFESSIONNEL DES VINS DE PROVENCE</p>
+    <div class="">
+      <p class="address-info">MAISON DES VINS</p>
+      <p class="address-info">RN 7 - 83460 LES ARCS SUR ARGENS</p>
+      <p>Tél : <a href="tel:0634789201">0634789201</a></p>
+      <p>Tél : <a href="fax:0134789201">0134789201</a></p>
+      <p>Mail : <a href="mailto:">vins@provence.fr</a></p>
+    </div>
+  </div>
+</div>
+
+
+  <div>
+  <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+  viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+  <defs>
+  <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+  </defs>
+  <g class="parallax">
+  <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(65, 47, 89,0.7" />
+  <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(65, 47, 89,0.5)" />
+  <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(65, 47, 89,0.3)" />
+  <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+  </g>
+  </svg>
+  </div>
+</section>
---
+++ resources/views/components/show_obligation.blade.php
@@ -0,0 +1,36 @@
+<div class="modal fade" id="ModalSObligation" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+          <span class="triangle-color"></span>
+          <div class="">
+          </div>
+          <div class=""><i class="far fa-bell"></i>
+          </div>
+
+          <h4 class="modal-title" id="head_title" style="font-weight:bold;padding:5px">Obligation</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="">
+            <label for=""></label>
+            <h5>Note</h5>
+            <div class="">
+              <p></p>
+            </div>
+          </div>
+
+          <div class="modal-footer">
+            <h5>Date of Event</h5>
+            <h5>Nov 02 2019 - Nov 03 2019</h5>
+          </div>
+          <div class="">
+            <a href="#">Qu'est-ce qu'en dit la loi ?</a>
+          </div>
+        </form>
+        </div>
+    </div>
+  </div>
+</div>
