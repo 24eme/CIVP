a767cc78e9ae910a35be82c8a1cf8e8088a9798b
Mame <mngom@24eme.fr>
2021-01-28 21:34:33 +0100

    Ajout du Model Event(Controller,Migration)

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class EventController extends Controller
+{
+    // public function show($q){
+    //   $query = $q;
+    //   $results = DB::select('select * from events where title = ?',[$query]);
+    //
+    //   return redirect()->back()->with('resultats', $results);
+    // }
+}
---
+++ database/migrations/2021_01_27_123405_create_evenements_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEvenementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evenements', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('title');
+          $table->string('start');
+          $table->string('end');
+          $table->string('description');
+          $table->string('contact')->nullable();
+          $table->string('color');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evenements');
+    }
+}
