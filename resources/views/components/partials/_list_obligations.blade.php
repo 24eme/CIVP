f4be0037bf1d1d35b8bb7f07a73800231872619a
Mame <mngom@24eme.fr>
2021-01-28 11:17:07 +0100

    Ajout BTN IMPORT sans fonction

--- resources/views/components/partials/_list_obligations.blade.php
+++ resources/views/components/partials/_list_obligations.blade.php
@@ -1,22 +1,37 @@
-<div id="LObligations" class="section-content listed-container">
-  <ul class="list_titles">
-    <li class="list_title"><a href="#obligations">Obligations</a></li>
-    <li class="list_title"><a href="#calendrier">Exemple</a></li>
-    <li class="list_title"><a href="#site">Statis</a></li>
+<div class="">
+  <ul>
+    <li></li>
+    <li></li>
+    <li></li>
   </ul>
-  <ul class="">
-    <input type="file" name="" value="">
+</div>
+<div id="LObligations" class="section-content listed-container">
+<ul class="">
+  <div class="download_csv">
+    <form class="" action="{{route('importCSV')}}" method="post">
+      <h2></i>Importer via fichier CSV</h2>
+      <label id="csv_label" for="csv_file"><i class="fas fa-cloud-download-alt"></i></label>
+      <input id="csv_input" type="file" name="" value="">
+    </form>
+  </div>
+  <h3>Les obligations activés</h3>
+    <hr>
+  <div class="obligations-container">
   @foreach($obligations as $obligation)
-    <li class="obligation-container">
+    <li class="obligation-content">
       <span class="vertical-line">{{ date('j M Y', strtotime($obligation->start)) }}</span>
       <div class="wrapper">
         <span>{{$obligation->title}}</span>
-        <span>{{$obligation->contact}} Exemple</span>
-      </div>
-      <div class="wrapper" style="float:right">
-
+        <span>{{$obligation->start}}</span>
       </div>
     </li>
   @endforeach
-  </ul>
+  </div>
+  <h3>Les obligations désactivés</h3>
+    <hr>
+  @foreach($obligations as $obligation)
+    @if($obligation->color =='blue')
+    @endif
+  @endforeach
+</ul>
 </div>
