a5e2474e1ce62415a80f0e95dd0f0e20349f8122
Mame <mngom@24eme.fr>
2021-01-29 10:09:37 +0100

    Potentiel Page Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('content')
+    <div id="main">
+      Les Vins de Provence sont fiers de vous présenter un Calendrier destinés aux différents familles viticules mais également
+      particuliers amateurs de Vins
+    </div>
+@endsection
