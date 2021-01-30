fd9ea42fc5dbac7aa089d9f8582fe3438b3f04a0
Mame <mngom@24eme.fr>
2021-01-30 14:14:48 +0100

    Debug

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -11,11 +11,11 @@ class CalendarController extends Controller
 {
     public function manage(){
       $obligations = Obligation::all();
-      return view('admin',['obligations'=>$obligations]);
+      return view('admin/admin',['obligations'=>$obligations]);
     }
 
 
-    function importCSV(Request $request){
+    public function importCSV(Request $request){
 
     $csv = fopen($request->file('csv_file'),"r");
     while(!feof($csv)){
@@ -36,66 +36,4 @@ class CalendarController extends Controller
 
     }
 
-    // return redirect()->back()->with('success', 'Votre obligation a été ajouté');
-
-    // $csv = fopen($request->file('csv_file'),"r");
-    // while($data = fgetcsv($csv, 1000, ";")){
-    //   dd($data);
-    //     // $inserted_data=array('id'=>$dat[0],
-    //     //                      'title'=>$value[1],
-    //     //                      'start'=>$value[2],
-    //     //                      'end'=>$value[3],
-    //     //                     );
-    // }
-    // $csv_path = $request->file('csv_file')->path();
-    // $options = array();
-    // extract($options);
-    // $delimiter = ';';
-    // if (($csv_handle = fopen($csv_path, "r")) === FALSE)
-    //     throw new Exception('Impossible d\'ouvrir ce fichier CSV');
-    //
-    // if(!isset($delimiter))
-    //     $delimiter = ',';
-    //
-    // if(!isset($table))
-    //     $table = preg_replace("/[^A-Z0-9]/i", '', basename($csv_path));
-    //
-    // if(!isset($fields)){
-    //     $fields = array_map(function ($field){
-    //         return strtolower(preg_replace("/[^A-Z0-9]/i", '', $field));
-    //     }, fgetcsv($csv_handle, 0, $delimiter));
-    // }
-    //
-    // $create_fields_str = join(', ', array_map(function ($field){
-    //     return "$field TEXT NULL";
-    // }, $fields));
-    //
-    // $pdo->beginTransaction();
-    //
-    // $create_table_sql = "CREATE TABLE IF NOT EXISTS $table ($create_fields_str)";
-    // $pdo->exec($create_table_sql);
-    //
-    // $insert_fields_str = join(', ', $fields);
-    // $insert_values_str = join(', ', array_fill(0, count($fields),  '?'));
-    // $insert_sql = "INSERT INTO $table ($insert_fields_str) VALUES ($insert_values_str)";
-    // $insert_sth = $pdo->prepare($insert_sql);
-    //
-    // $inserted_rows = 0;
-    // while (($data = fgetcsv($csv_handle, 0, $delimiter)) !== FALSE) {
-    //     $insert_sth->execute($data);
-    //     $inserted_rows++;
-    // }
-    //
-    // $pdo->commit();
-    //
-    // fclose($csv_handle);
-    //
-    // return array(
-    //         'table' => $table,
-    //         'fields' => $fields,
-    //         'insert' => $insert_sth,
-    //         'inserted_rows' => $inserted_rows
-    //     );
-    //
-    // }
 }
