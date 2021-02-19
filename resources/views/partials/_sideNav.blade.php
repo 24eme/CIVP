492f60ffa3c75128ac43873c2b1fe2b3f0129481
Mame <mngom@24eme.fr>
2021-02-19 16:45:07 +0100

    CIVP:Modification Navigation(User)

--- resources/views/partials/_sideNav.blade.php
+++ resources/views/partials/_sideNav.blade.php
@@ -1,17 +1,40 @@
-<div id="side_navigation" class="side_navigation">
-    <!-- <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"><i class="fas fa-times"></i></a> -->
-    <h4 class="side_nav_h">FILTRER PAR </h4><br>
-    <h6 class="side_nav_h">FAMILLE </h5>
-  <div class="filter-wrapper">
-    <div class="filter-div"><label class="filter-label" for="FilterProdRec">Producteur - Récoltant</label></div><label class="switch"><input class="filter-profil" id="FilterProdRec" name="producteur" value="Producteur-Recoltant" type="checkbox" checked><span id="sliderProd" class="slider round"></span></label>
-    <div class="filter-div"><label class="filter-label" for="FilterNegociant">Négociant</label></div><label class="switch"><input class="filter-profil" id="FilterNegociant" name="nego" value="Negociant" type="checkbox" checked><span id="sliderNego" class="slider round"></span></label>
-    <div class="filter-div"><label class="filter-label" for="FilterNegociantVini">Négociant - Vinificateur</label></div><label class="switch"><input class="filter-profil" id="FilterNegociantVini" name="nego" value="Negociant-Vinificateur" type="checkbox" checked><span id="sliderVini" class="slider round"></span></label>
-    <div class="filter-div"><label class="filter-label" for="FilterViticulteur">Viticulteur ou exploitant agricole</label></div><label class="switch"><input class="filter-profil" id="FilterViticulteur" name="viti" value="Viticulteur" type="checkbox" checked><span id="sliderViti" class="slider round"></span></label>
+<div id="sideNavigation" class="sideNavigation">
+  <div class="row upper_wrapper">
+    <div class="col-md-12">
+      <a href="/"><span style="display: inline-block;margin-left:20px;"> <img src="{{asset('images/logos/logo-C.png')}}" width=50  alt=""></span></a>
+      <a href="/"><p style="font-size:0.9em" class="upper-title">LE CALENDRIER</p></a>
+    </div>
   </div>
-    <h5 class="side_nav_h">TAG</h5>
-    <ul>
-      <li><a href="#" class="tag">CIVP</a></li>
-      <li><a href="#" class="tag">IGP</a></li>
-      <li><a href="#" class="tag">AIDES</a></li>
-    </ul>
+  <div class="row p-4 mt-5 FiltrerPar">
+    <div class="col-md-12 p-0">
+      <h4>Filtrer par</h4>
+    </div>
+  </div>
+  <div class="row ml-5">
+    <div class="col-md-12 mt-2 p-0">
+      <h5>Famille</h5>
+    </div>
+  </div>
+  <div class="row ml-0">
+    <div class="col-md-12">
+        <div class="filter-wrapper">
+          <div class="filter-div"><label class="filter-label" for="FilterProdRec">Producteur - Récoltant</label></div><label class="switch"><input class="filter-profil" id="FilterProdRec" name="producteur" value="Producteur-Recoltant" type="checkbox" checked><span id="sliderProd" class="slider round"></span></label>
+          <div class="filter-div"><label class="filter-label" for="FilterNegociant">Négociant</label></div><label class="switch"><input class="filter-profil" id="FilterNegociant" name="nego" value="Negociant" type="checkbox" checked><span id="sliderNego" class="slider round"></span></label>
+          <div class="filter-div"><label class="filter-label" for="FilterNegociantVini">Négociant - Vinificateur</label></div><label class="switch"><input class="filter-profil" id="FilterNegociantVini" name="nego" value="Negociant-Vinificateur" type="checkbox" checked><span id="sliderVini" class="slider round"></span></label>
+          <div class="filter-div"><label class="filter-label" for="FilterViticulteur">Viticulteur (Exploitant agricole)</label></div><label class="switch"><input class="filter-profil" id="FilterViticulteur" name="viti" value="Viticulteur" type="checkbox" checked><span id="sliderViti" class="slider round"></span></label>
+        </div>
+    </div>
+  </div>
+  <div class="row ml-5 mt-5">
+    <div class="col-md-12">
+      <h5>TAG</h5>
+    </div>
+  </div>
+  <div class="row ml-4 mt-1">
+      <ul class="tagList p-2">
+        <li class="tag-item"><a href="#" class="tag">CIVP</a></li>
+        <li class="tag-item"><a href="#" class="tag">IGP</a></li>
+        <li class="tag-item"><a href="#" class="tag">AIDES</a></li>
+      </ul>
+  </div>
 </div>
