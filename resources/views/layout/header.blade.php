e550510ac42b79f1d9b0c098c9b580c63a6b3c25
Mame <mngom@24eme.fr>
2021-02-19 16:43:57 +0100

    CIVP: Modification header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,19 +1,19 @@
 <div id="upper_nav">
-  <div class="upper-wrapper">
-    <a href="/"><span style="float:left;"> <img src="{{asset('images/logos/logo-P.svg')}}" width=50  alt=""></span></a>
-  </div>
-  <div class="upper-wrapper">
-      <a href="/"><p style="font-size:0.8em" class="upper-title">CALENDRIER DE PROVENCE</p></a>
-  </div>
-  <div class="upper-wrapper btn-wrapper">
-        <button id="btn-ListView"class="btn btn-upper-title" type="button" name="button"><a href="#">LISTE</a></button>
-        <div class="circle-separator"></div>
-        <button id="btn-DayGrid"class="btn btn-upper-title" type="button" name="button"><a href="/">CALENDRIER</a></button>
-        <div class="circle-separator"></div>
-        <button id="btn-TimelineView"class="btn btn-upper-title" type="button" name="button"><a href="/">TIMELINE</a></button>
-        <div class="circle-separator"></div>
-        <button class="btn-openFilter" type="button" name="button" onclick="openNav()">MENU</button>
-        <div class="circle-separator"></div>
-        <button class="btn-openFilter btn-search" type="button" name="button" onclick="openEvents()">RECHERCHER</button>
+  <div class="row">
+    <div class="col-md-2">
+      <div class="upper_wrapper">
+        <button class="btn-openFilter" type="button" name="button" onclick="openNav()"><i class="fas fa-bars" style="font-size:1.7em;"></i></button>
+      </div>
+    </div>
+    <div class="col-md-8">
+      <div class="upper_wrapper" style="margin-left:15%">
+        <i class="fas fa-search" style="color:grey;position:absolute;margin:10px;margin-top:8px;"></i><input class="input-search" type="text" placeholder="Recherche..." name="" value="" onkeyup="openEvents()" onclick="openEvents()">
+      </div>
+    </div>
+    <div class="col-md-2">
+      <div class="upper_wrapper">
+        <button id="btn-DayGrid" class="btn btn-secondary ml-4" type="button" name="button" onclick="openEvents()"><a style="color:white;">Evenements</a></button>
+      </div>
+    </div>
   </div>
 </div>
