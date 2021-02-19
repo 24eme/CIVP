3f5a84039a398e36fd690cc784654db1bb063605
Mame <mngom@24eme.fr>
2021-02-19 16:39:54 +0100

    CIVP:Changement affichage Event

--- resources/views/admin/event/_list_events.blade.php
+++ resources/views/admin/event/_list_events.blade.php
@@ -1,10 +1,33 @@
-<div id="events_side" class="events_list">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeEvents()"><i class="fas fa-times"></i></a>
-    <h4>Evenements</h4>
-    <hr>
-    <input type="search" id="inputSearch" class="input-search" onkeyup="searchEvents()" placeholder="Rechercher par nom" title="Type in a name">
-
-    <ul id="events_list">
-      <li></li>
-    </ul>
+<div id="eventNavigation" >
+  <div class="row">
+    <div class="col-md-1">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeEvents()"><i class="fas fa-chevron-circle-right"></i></a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <input type="search" id="inputSearch" class="input-search" onkeyup="searchEvents()" placeholder=" Recherche..." title="Type in a name">
+    </div>
+  </div>
+  <div class="row m-2">
+    <div class="col-md-12">
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          TYPE D'EVENEMENTS
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#">Conférence</a>
+          <a class="dropdown-item" href="#">Rencontre</a>
+          <a class="dropdown-item" href="#">Atelier</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <ul id="eventList">
+        <li></li>
+      </ul>
+    </div>
+  </div>
 </div>
