04ef844783bd0400c49316bf0441487cd4555d9f
Mame <mngom@24eme.fr>
2021-02-19 16:40:40 +0100

    CIVP:Front->Changement affichage Obligation

--- resources/views/admin/obligation/_list_obligations.blade.php
+++ resources/views/admin/obligation/_list_obligations.blade.php
@@ -10,22 +10,51 @@
     <form class="" action="{{route('importCSV')}}" method="post" enctype="multipart/form-data">
       @csrf
       <div class="btn-import">
-        <label id="csv_label" for="csv_input">Importer <i class="fas fa-cloud-download-alt"></i></label>
+        <label id="csv_label" for="csv_input">Importer un fichier CSV<i class="fas fa-cloud-download-alt"></i></label>
         <input id="csv_input" type="file"  name="csv_file" onchange="form.submit()">
       </div>
     </form>
   </div>
   <div class="obligations-container">
-  <ul class="">
-  @foreach($obligations as $obligation)
-    <li class="obligation-content">
-      <span class="vertical-line">{{ date('j M Y', strtotime($obligation->start)) }}</span>
-      <div class="wrapper">
-        <span class="obligation_title">{{$obligation->title}}  <span class="active_dot"></span><p class="active_text">active</p></span>
-        <span id="obligation_start" class="obligation_date">Date de début : {{$obligation->start}}</span>
-        <span id="obligation_end" class="obligation_date">Date de fin : {{$obligation->end}}</span>
+  <ul id="obligationList" class="p-0">
+    <li class="obligation-content pl-3 pt-2 pb-0" style="background-color:white;">
+      <div class="row p-2" style="color:#4c5d72">
+        <div class="col-md-4 pl-2">
+          <h5>TITRE</h5>
+        </div>
+        <div class="col-md-3 p-0">
+          <h5>DATE DE DEBUT</h5>
+        </div>
+        <div class="col-md-3">
+          <h5>DATE DE FIN</h5>
+        </div>
+        <div class="col-md-2">
+          <h5>STATUT</h5>
+        </div>
       </div>
-
+    </li>
+  @foreach($obligations as $obligation)
+    <li class="obligation-content" >
+      <div class="row">
+        <div class="col-md-4">
+          {{$obligation->title}}
+        </div>
+        <div class="col-md-auto p-0">
+          <i class="far fa-clock"></i>
+        </div>
+        <div class="col-md-3 p-0">
+          {{$obligation->start}}
+        </div>
+        <div class="col-md-auto p-0">
+          <i class="far fa-clock"></i>
+        </div>
+        <div class="col-md-2 p-0">
+          {{$obligation->end}}
+        </div>
+        <div class="col-md-2 ml-5">
+          <span class="active_dot"></span><p class="active_text">active</p>
+        </div>
+      </div>
     </li>
   @endforeach
   </ul>
