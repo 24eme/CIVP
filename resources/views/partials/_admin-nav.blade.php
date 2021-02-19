f674464ad0f196a22dfd40bc71d6e30e8c7ae93d
Mame <mngom@24eme.fr>
2021-02-19 16:44:40 +0100

    CIVP: Modification Navigation(Admin)

--- resources/views/partials/_admin-nav.blade.php
+++ resources/views/partials/_admin-nav.blade.php
@@ -1,11 +1,19 @@
-<div id="admin_nav" class="side_navigation">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"><i class="fas fa-times"></i></a>
-  <ul class="admin-navbar">
-    <a href="#admin"><li><button class="btn-admin" type="button" name="button"><i class="fas fa-chart-line"></i>Tableau de Bord</button></li></a>
-    <a href="#obligations"><li><button class="btn-admin" type="button" name="button"><i class="far fa-calendar-check"></i>Les obligations</button></li></a>
-    <a href="#calendrier"><li><button class="btn-admin" type="button" name="button"><i class="far fa-calendar"></i>Gérer le Calendrier</button></li></a>
-    <a href="#filtres"><li><button class="btn-admin" type="button" name="button"><i class="fas fa-sliders-h"></i>Gérer les filtres</button></li></a>
-    <a href="#contact"><li><button class="btn-admin" type="button" name="button"><i class="far fa-address-book"></i>Contact</button></li></a>
-    <a href="#"><li><button class="btn-admin" type="button" name="button" data-toggle="modal" data-target="#ModalCObligation"><i class="fas fa-plus"></i>Créer une obligation</button></li></a>
-  </ul>
+<div id="adminNavigation" class="sideNavigation">
+  <div class="row upper_wrapper">
+    <div class="col-md-12">
+      <a href="/"><span style="display: inline-block;margin-left:20px;"> <img src="{{asset('images/logos/logo-C.png')}}" width=50  alt=""></span></a>
+      <a href="/"><p style="font-size:0.9em" class="upper-title">LE CALENDRIER</p></a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12 mt-5">
+      <ul class="admin-navbar">
+        <a href="#"><li><button class="btn-admin" type="button" name="button" data-toggle="modal" data-target="#ModalCObligation"><i class="fas fa-plus"></i>Créer une obligation</button></li></a>
+        <a href="#calendrier"><li><button class="btn-admin" type="button" name="button"><i class="far fa-calendar"></i>Calendrier</button></li></a>
+        <a href="#obligations"><li><button class="btn-admin" type="button" name="button"><i class="far fa-calendar-check"></i>Les obligations</button></li></a>
+        <a href="#filtres"><li><button class="btn-admin" type="button" name="button"><i class="fas fa-sliders-h"></i>Gérer les filtres</button></li></a>
+        <a href="#contact"><li><button class="btn-admin" type="button" name="button"><i class="far fa-address-book"></i>Contact</button></li></a>
+      </ul>
+    </div>
+  </div>
 </div>
