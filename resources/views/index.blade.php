c2a8514801bb358b5c77e614d0f7c2e0854d272c
Mame <mngom@24eme.fr>
2021-03-01 17:07:53 +0100

    CIVP : Retrait TEMPORAIRE du sous menu pour SPRINT

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
       @include('admin/event/_list_events')
     <div id="main">
       @include('layout/header')
-      <div class="row mt-1">
+      <!-- <div class="row mt-1">
         <div class="col-md-auto ml-auto p-0">
           <button id="btn-ListView"class="btn-upper" type="button" name="button"><a href="/#liste">LISTE</a></button>
         </div>
@@ -22,7 +22,7 @@
         <div class="col-md-auto mr-auto p-0">
           <button id="btn-TimelineView"class="btn-upper" type="button" name="button"><a href="/#timeline">TIMELINE</a></button>
         </div>
-      </div>
+      </div> -->
       <div id='calendar'></div>
       <div class="obligation_non_dates" style="display:none;">
         <h3>Obligations non-daté</h3>
