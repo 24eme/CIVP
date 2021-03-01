6e85e1af3f447e365de3c52fb5b1e9facb6114da
Mame <mngom@24eme.fr>
2021-03-01 16:43:15 +0100

    CIVP : Ajout ListGroup sans données

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -7,7 +7,14 @@
     </div>
     <div class="col-md-8">
       <div class="upper_wrapper" style="margin-left:15%">
-        <i class="fas fa-search" style="color:grey;position:absolute;margin:10px;margin-top:8px;"></i><input class="input-search" type="text" placeholder="Recherche..." name="" value="" >
+        <i class="fas fa-search" style="color:grey;position:absolute;margin:10px;margin-top:8px;"></i><input class="input-search" type="text" placeholder="Recherche..." onkeypress="openListGroup()" name="" value="" >
+        <ul id="ListGroupSearch" class="list-group list-group-custom">
+          <li class="list-group-item">Cras justo odio</li>
+          <li class="list-group-item">Dapibus ac facilisis in</li>
+          <li class="list-group-item">Morbi leo risus</li>
+          <li class="list-group-item">Porta ac consectetur ac</li>
+          <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
       </div>
     </div>
     <div class="col-md-2">
