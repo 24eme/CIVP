09f50fe9e7fb10d4c462d6730e1dc99a7f962dc4
Mame <mngom@24eme.fr>
2021-03-02 10:58:32 +0100

    CIVP:Ajout des scripts rrule (Recurrence)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Calendrier - CIVP</title>
         <link rel="shortcut icon" href="{{ asset('images/logos/logo-P.svg') }}" >
+        <script src='https://cdn.jsdelivr.net/npm/rrule@2.6.4/dist/es5/rrule.min.js'></script>
+        <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.5.0/main.min.js'></script>
+        <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/rrule@5.5.0/main.global.min.js'></script>
         <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.5.0/main.min.css' rel='stylesheet' />
         <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.5.0/main.min.js'></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
