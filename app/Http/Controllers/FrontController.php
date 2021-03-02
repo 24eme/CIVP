8d08dc7c05d3fc2abf5af77533c23e434ce58c2d
Jb Lm <lemetayer.jb@gmail.com>
2021-03-02 23:52:36 +0100

    gestion de la vue liste

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -5,13 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\Evenement;
+use App\Models\Tag;
+use App\Models\Famille;
+use App\Models\Organisme;
+use App\Models\Profil;
 
 class FrontController extends Controller
 {
     public function index()
     {
       $evenements = Evenement::get()->all();
-      return view('index', ['evenements' => $evenements]);
+      $tags = Tag::get()->all();
+      $familles = Famille::get()->all();
+      $organismes = Organisme::get()->all();
+      $profils = Profil::get()->all();
+      return view('index', ['evenements' => $evenements,'tags' => $tags, 'familles' => $familles, 'organismes' => $organismes, 'profils' => $profils]);
     }
 
     public function evenements()
