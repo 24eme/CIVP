0e486b3083b29ebfe6033c6d59eb55364d6356a3
Mame <mngom@24eme.fr>
2021-03-03 20:45:29 +0100

    CIVP:Changement du titre

--- resources/views/partials/_sideNav.blade.php
+++ resources/views/partials/_sideNav.blade.php
@@ -2,7 +2,7 @@
   <div class="row upper_wrapper">
     <div class="col-md-12">
       <a href="/"><span style="display: inline-block;margin-left:20px;"> <img src="{{asset('images/logos/logo-C.png')}}" width=50  alt=""></span></a>
-      <a href="/"><p style="font-size:0.9em" class="upper-title">Calendrier des obligations viticoles</p></a>
+      <a href="/"><p style="font-size:0.9em" class="upper-title">Calendrier des déclarations viti/vinicoles</p></a>
     </div>
   </div>
   <div class="row p-0 mt-5">
@@ -28,7 +28,7 @@
   <div class="row ml-0">
     <div class="col-md-12">
         <div class="filter-wrapper">
-          <div class="filter-div"><label class="filter-label" for="FilterProdRec">Obligation</label></div><label class="switch"><input class="filter-profil" id="FilterProdRec" name="producteur" value="Producteur-Recoltant" type="checkbox" checked><span id="sliderProd" class="slider round"></span></label>
+          <div class="filter-div"><label class="filter-label" for="FilterProdRec">Producteur - Exploitant</label></div><label class="switch"><input class="filter-profil" id="FilterProdRec" name="producteur" value="Producteur-Recoltant" type="checkbox" checked><span id="sliderProd" class="slider round"></span></label>
           <div class="filter-div"><label class="filter-label" for="FilterNegociant">Négociant</label></div><label class="switch"><input class="filter-profil" id="FilterNegociant" name="nego" value="Negociant" type="checkbox" checked><span id="sliderNego" class="slider round"></span></label>
           <div class="filter-div"><label class="filter-label" for="FilterNegociantVini">Négociant - Vinificateur</label></div><label class="switch"><input class="filter-profil" id="FilterNegociantVini" name="nego" value="Negociant-Vinificateur" type="checkbox" checked><span id="sliderVini" class="slider round"></span></label>
           <div class="filter-div"><label class="filter-label" for="FilterViticulteur">Viticulteur (Exploitant agricole)</label></div><label class="switch"><input class="filter-profil" id="FilterViticulteur" name="viti" value="Viticulteur" type="checkbox" checked><span id="sliderViti" class="slider round"></span></label>
