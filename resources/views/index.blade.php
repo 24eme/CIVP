7798621b420c15cb31d732c8c1227231329b5e18
Mame <mngom@24eme.fr>
2021-03-10 21:56:42 +0100

    MODIFIED:ID plus parlant

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,31 +1,25 @@
 @extends('layout')
-@include('calendar/calendar')
-
+@include('partials/_sideNav')
 @section('content')
+<div id="main" class="main">
+  <nav class="mt-4">
+    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+      <a class="nav-link active" id="nav-calendrier-tab" data-toggle="tab" href="#nav-calendrier" role="tab" aria-controls="nav-calendrier" aria-selected="true">Calendrier</a>
+      <a class="nav-link" id="nav-liste-tab" data-toggle="tab" href="#nav-liste" role="tab" aria-controls="nav-liste" aria-selected="false">Liste</a>
+      <a class="nav-link" id="nav-timeline-tab" data-toggle="tab" href="#nav-timeline" role="tab" aria-controls="nav-timeline" aria-disabled="true">Timeline</a>
+    </div>
+  </nav>
+  <div class="tab-content mt-4" id="nav-tabContent">
+    <div class="tab-pane fade show active" id="nav-calendrier" role="tabpanel" aria-labelledby="nav-calendrier-tab">
+      <div id='calendar'></div>
+    </div>
+    <div class="tab-pane fade" id="nav-liste" role="tabpanel" aria-labelledby="nav-liste-tab">
+      <div id='calendar-list'></div>
+    </div>
+    <div class="tab-pane fade" id="nav-timeline" role="tabpanel" aria-labelledby="nav-timeline-tab">
+      @include('timeline/timeline')
+    </div>
+  </div>
+</div>
 
-      @include('partials/_sideNav')
-
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-
-
-      <nav>
-        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-          <a class="nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Calendrier</a>
-          <a class="nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Liste</a>
-          <a class="nav-link disabled" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-disabled="true">Timeline</a>
-        </div>
-      </nav>
-      <div class="tab-content" id="nav-tabContent">
-        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab"><div id='calendar'></div></div>
-        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">@include('evenement/_list_events')</div>
-        <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab"></div>
-      </div>
-
-
-
-
-
-    </main>
-    @include('layout/popup')
 @endsection
-@include('timeline/timeline')
