9186edbbd6390ccf7da54990e4d0b486075f5194
Mame <mngom@24eme.fr>
2021-03-10 21:55:28 +0100

    MODIFIED:Suppression du contenu inutile

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,6 +1,1 @@
-<header class="navbar flex-md-nowrap p-0">
-  <a class="navbar-brand me-0 px-3 py-3" href="#"><img src="{{asset('images/logos/logo-C.png')}}" width=50  alt="" />&nbsp;Calendrier des obligations viticoles</a>
-  <button class="navbar-toggler float-right d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-</header>
+
