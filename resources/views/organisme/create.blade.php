223a6b7178f8eae2921b5d846ce5aeb974c35135
Mame <mngom@24eme.fr>
2021-03-10 21:53:07 +0100

    DEBUG:Changement de layout

--- resources/views/organisme/create.blade.php
+++ resources/views/organisme/create.blade.php
@@ -1,5 +1,4 @@
-@extends('layoutAdmin')
-
+@extends('admin/layout')
 @section('content')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
   <h1 class="h4">Ajout d'un organisme</h1>
