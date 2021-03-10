1dccc62104d12808af768f90c311c39f44d0bbd2
Mame <mngom@24eme.fr>
2021-03-10 22:05:12 +0100

    DEBUG;

---
+++ app/Http/Controllers/CalendarController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\Evenement;
+use App\Models\Tag;
+use App\Models\Famille;
+use App\Models\Organisme;
+use App\Models\Profil;
+use Illuminate\Http\UploadedFile;
+
+class CalendarController extends Controller
+{
+    public function index()
+    {
+      $evenements = Evenement::get()->all();
+      $familles = Famille::all();
+      $organismes = Organisme::all();
+      $profils = Profil::all();
+      $tags = Tag::all();
+      return view('admin/index', ['evenements' => $evenements,'tags' => $tags, 'familles' => $familles, 'organismes' => $organismes, 'profils' => $profils]);
+    }
+
+    public function importCSV(Request $request){
+
+    $csv = fopen($request->file('csv_file'),"r");
+    while(!feof($csv)){
+      $rowData[]=fgetcsv($csv,3000,";");
+    }
+      foreach ($rowData as $key => $value ) {
+        if ($value !==false) {
+        $inserted_data=array('title'=> $value[0],
+                             'start'=> $value[1],
+                             'end'=> $value[2],
+                             'description'=> $value[3],
+                             'profil_id'=> $value[4],
+                             'organisme_id' => $value[5],
+                             'textedeloi' => $value[6],
+                             'liendeclaration' => $value[7],
+                             'rrule' => $value[8],
+                            );
+        Evenement::create($inserted_data);
+        }
+      }
+      return redirect()->back()->with('success', 'Votre obligation a été ajouté');
+
+    }
+
+}
