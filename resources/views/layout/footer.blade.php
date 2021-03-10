11984008cf9ed9432c5bafd606f4152a29e7404c
Mame <mngom@24eme.fr>
2021-03-10 21:54:19 +0100

    MODIFIED:Nouveau footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,27 @@
-<footer id="footer">
-    <ul class="nav justify-content-center" role="menubar">
-    <li class="nav-item" id="nav-item-civp"><a class="nav-link" id="nav-link-civp" href="#">CIVP</a></li>
-    <li class="nav-item" id="nav-item-contact"><a class="nav-link" id="nav-link-contact" href="#">CONTACT</a></li>
-    <li class="nav-item" id="nav-item-mentions-legales"><a class="nav-link" id="nav-link-mentions-legales" href="#">MENTIONS LEGALES</a></li>
-  </ul>
+<footer class="bg-dark text-center text-white">
+  <div class="container p-4">
+    <section class="mb-4">
+      <p>Les Vins de Provence sont fiers de vous présenter un Calendrier destinés aux différents familles viticules mais également particuliers amateurs de Vins</p>
+    </section>
+    <section class="">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">CIVP</h5>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">A PROPOS</h5>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">CONTACT</h5>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">MENTIONS LEGALES</h5>
+        </div>
+      </div>
+    </section>
+  </div>
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2021 Vins de Provence:
+    <a class="text-white" href="https://www.vinsdeprovence.com/">vinsdeprovence.com</a>
+  </div>
 </footer>
