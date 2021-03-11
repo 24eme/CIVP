6c8e6907dfbc23bff2eda2465b40947c21e47dec
Mame <mngom@24eme.fr>
2021-03-11 19:22:00 +0100

    Suppression de border

--- resources/views/organisme/list.blade.php
+++ resources/views/organisme/list.blade.php
@@ -1,7 +1,7 @@
 @extends('admin/layout')
 
 @section('content')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 ">
   <h1 class="h3">Organisme</h1>
   <a href="{{ route('organisme_create') }}" class="btn btn-warning float-right"><i class="fas fa-plus">&nbsp;Nouveau</i></a>
 </div>
