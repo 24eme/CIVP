6d8e4dde9260092a33ce04431a6fa919fd0067ef
Mame <mngom@24eme.fr>
2021-03-11 12:59:47 +0100

    Suppression ancien menu

--- resources/views/partials/_adminNav.blade.php
+++ resources/views/partials/_adminNav.blade.php
@@ -28,22 +28,3 @@
   </ul>
 </div>
 </nav>
-<!-- <div id="adminNavigation" class="container navbar-custom">
-  <div class="row upper_wrapper">
-    <div class="col-md-12">
-      <a href="/"><p class="">Administrateur</p></a>
-      <a href="/"><p class="">Caroline</p></a>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-12 mt-5">
-      <ul class="admin-navbar">
-        <a href="#"><li><button class="btn-admin" type="button" name="button" data-toggle="modal" data-target="#ModalCObligation"><i class="fas fa-plus"></i>Créer une obligation</button></li></a>
-        <a href="#calendrier"><li><button class="btn-admin" type="button" name="button"><i class="far fa-calendar"></i>Calendrier</button></li></a>
-        <a href="#obligations"><li><button class="btn-admin" type="button" name="button"><i class="far fa-calendar-check"></i>Les obligations</button></li></a>
-        <a href="#filtres"><li><button class="btn-admin" type="button" name="button"><i class="fas fa-sliders-h"></i>Gérer les filtres</button></li></a>
-        <a href="#contact"><li><button class="btn-admin" type="button" name="button"><i class="far fa-address-book"></i>Contact</button></li></a>
-      </ul>
-    </div>
-  </div>
-</div> -->
