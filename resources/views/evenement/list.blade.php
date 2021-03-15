32260dd567f0d42a77b1e774067e2ceb9845bd7c
Mame <mngom@24eme.fr>
2021-03-15 12:24:31 +0100

    Ajout informations evenement

--- resources/views/evenement/list.blade.php
+++ resources/views/evenement/list.blade.php
@@ -1,64 +1,22 @@
-@extends('admin/layout')
-@section('content')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 my-3">
-  <h1 class="h3">Evènements</h1>
-  <a href="{{ route('evenement_create') }}" class="btn btn-warning float-right"><i class="fas fa-plus">&nbsp;Nouveau</i></a>
-</div>
-<div id="LObligations" class="row section-content listed-container mb-3">
-  <div class="col-md-3 mx-auto download_csv">
-    <form class="" action="{{route('importCSV')}}" method="post" enctype="multipart/form-data">
-      @csrf
-      <div class="mx-auto btn-import">
-        <label id="csv_label" for="csv_input">Importer un fichier CSV<i class="fas fa-cloud-download-alt"></i></label>
-        <input id="csv_input" type="file"  name="csv_file" onchange="form.submit()">
-      </div>
-    </form>
-  </div>
-</div>
-@if (count($evenements) === 0)
-<p>
-  <i>Aucun événements créé</i>
-</p>
-@else
-<table class="table table-hover evenements-container">
-  <thead style="text-align:center">
-    <tr class="evenement-content">
-      <th class="col-md-auto">Identifiant</th>
-      <th class="col-md-auto">Type</th>
-      <th class="col-md-auto">Organisme</th>
-      <th class="col-md-auto">Titre</th>
-      <th class="col-md-auto"><i class="far fa-clock"></i> Date début</th>
-      <th class="col-md-auto"><i class="far fa-clock"></i> Date fin</th>
-      <th class="col-md-auto">&nbsp;</th>
+<h2 class="fc-toolbar-title"></h2>
+<div class="list-events">
+  <table class="fc-list-table table-bordered">
+    <tbody>
+    @foreach($evenements as $evenement)
+    <tr class="fc-list-day fc-day fc-day-tue fc-day-past">
+      <td class="fc-list-event-time"><a>{{ date('d F Y', strtotime($evenement->start)) }}</a></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
     </tr>
-  </thead>
-  <tbody>
-      @foreach($evenements as $evenement)
-      <tr class="evenement-content" style="text-align:center">
-      <td>
-        {{$evenement->id}}
-      </td>
-      <td>
-        <i class="fas fa-circle" style="color:{{$evenement->type->color}}"></i>&nbsp;{{$evenement->type->nom}}
-      </td>
-      <td>
-        {{$evenement->organisme->nom}}
-      </td>
-      <td>
-        <a href="javascript:void(0)" class="popupEvent" data-url="{{ route('evenement_popup', $evenement) }}"><strong>{{$evenement->title}}</strong></a>
-      </td>
-      <td> 
-        {{ date('d F Y', strtotime($evenement->start)) }}
-      </td>
-      <td>
-        {{ date('d F Y', strtotime($evenement->end)) }}
-      </td>
-      <td>
-        <a href="{{ route('evenement_edit', $evenement) }}"><i class="far fa-edit">&nbsp;</i></a>
-      </td>
-      </tr>
-      @endforeach
-  </tbody>
-</table>
-@endif
-@endsection
+    <tr class="fc-list-event  fc-event fc-event-start fc-event-end fc-event-past">
+      <td class="fc-list-event-time" style="width: 150px">En cours</td>
+      <td class="fc-list-event-title"><span class="fc-list-dot col-md-auto"><i class="fas fa-circle" style="color:{{$evenement->type->color}}"></i></span>{{$evenement->title}}</td>
+      <td class="fc-list-event-title">{{$evenement->type->nom}}</td>
+      <td class="fc-list-event-title" style="width: 300px">{{$evenement->organisme->nom}}</td>
+    </tr>
+    @endforeach
+    </tbody>
+  </table>
+</div>
