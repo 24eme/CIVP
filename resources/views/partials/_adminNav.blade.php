211864f49ec890da01b2cbaa43f26d5490c43a1a
Mame <mngom@24eme.fr>
2021-03-18 17:15:54 +0100

    Nouveau adminNav

--- resources/views/partials/_adminNav.blade.php
+++ resources/views/partials/_adminNav.blade.php
@@ -1,9 +1,9 @@
-<div class="row">
-  <div class="col-md-12 mt-5 p-0">
-    <ul class="admin-navbar p-0">
-      <a href="evenement/create"><li><button class="btn-admin" type="button" name="button" data-toggle="modal" data-target="#ModalCObligation"><i class="fas fa-plus"></i>Créer un evenement</button></li></a>
-      <a href="evenements"><li><button class="btn-admin active" type="button" name="button"><i class="far fa-calendar-check"></i>Evènements</button></li></a>
-      <a href="organismes"><li><button class="btn-admin" type="button" name="button"><i class="far fa-address-book"></i>Organismes</button></li></a>
-    </ul>
-  </div>
+<div class="row p-2 mt-3 FiltrerPar" style="border-color:#edbf37;">
+  <div class="col-md-12 pt-1"><a href="evenement/create"><h5 style="color:grey;">Créer un evenement</h5></a></div>
+</div>
+<div class="row p-2 mt-3 FiltrerPar" style="border-color:#b8cef2;">
+  <div class="col-md-12 pt-1"><a href="evenements"><h5 style="color:grey;">Evenements</h5></a></div>
+</div>
+<div class="row p-2 mt-3 FiltrerPar" style="border-color:#913f72;">
+  <div class="col-md-12 pt-1"><a href="organismes"><h5 style="color:grey;">Organismes</h5></a></div>
 </div>
