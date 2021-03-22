e2c5d8ea4c4420b30f3d6f0475eefff6b7c77052
Mame <mngom@24eme.fr>
2021-03-22 09:42:00 +0100

    Affichage liste

--- resources/views/partials/_list.blade.php
+++ resources/views/partials/_list.blade.php
@@ -1,47 +1,36 @@
+<div class="" hidden>
+@php($start = Carbon\Carbon::now()->startOfYear()->toDateString())
+@php($end =Carbon\Carbon::now()->endOfYear()->toDateString())
+@php($dates = Carbon\CarbonPeriod::create($start, $end))
+@php($done = [])
+</div>
 <h2 class="fc-toolbar-title"></h2>
 <div class="list-events">
   <table class="fc-list-table table-bordered">
     <tbody>
+      @foreach($evenements as $evenement)
+      @if(in_array($evenement->id, $done) == False)
       <tr class="fc-list-day fc-day fc-day-tue fc-day-past">
-        <td class="fc-list-event-time"><a>En cours</a></td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
+        <td class="fc-list-event-time col-md-auto"><a>{{ date('d F Y', strtotime($evenement->start )) }}</a></td>
+        <td class="col-md-auto">&nbsp;</td>
+        <td class="col-md-auto">&nbsp;</td>
+        <td class="col-md-auto">&nbsp;</td>
+        <td class="col-md-auto">&nbsp;</td>
       </tr>
-    @foreach($evenements as $evenement)
-    @if($evenement->active == true)
-    <tr class="fc-list-event  fc-event fc-event-start fc-event-end fc-event-past">
-      <td class="fc-list-event-time" style="width: 150px">{{ date('d F Y', strtotime($evenement->start)) }}</td>
-      <td class="fc-list-event-title"><span class="fc-list-dot col-md-auto"><i class="fas fa-circle" style="color:{{$evenement->type->color}}"></i></span>{{$evenement->title}}</td>
-      <td class="fc-list-event-title">{{$evenement->type->nom}}</td>
-      <td class="fc-list-event-title" style="width: 300px">{{$evenement->organisme->nom}}</td>
-      <td class="fc-list-event-title"><button type="button" class="btn btn-primary" onclick="exportICS({{$evenement->id}})"><i class="fas fa-external-link-alt"></i></button></td>
-    </tr>
-    @endif
-    @endforeach
-    </tbody>
-  </table>
-  <table class="fc-list-table table-bordered">
-    <tbody>
-      <tr class="fc-list-day fc-day fc-day-tue fc-day-past">
-        <td class="fc-list-event-time"><a>Terminé</a></td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
+      @endif
+      @foreach($evenements as $subevenement)
+      @if($subevenement->start == $evenement->start && in_array($subevenement->id, $done) == False)
+      @php(array_push($done,$subevenement->id))
+      <tr class="fc-list-event  fc-event fc-event-start fc-event-end fc-event-past">
+        <td class="fc-list-event-time col-md-1">{{ $subevenement->type->name }}</td>
+        <td class="fc-list-event-title col-md-6"><span class="fc-list-dot col-md-auto"><i class="fas fa-circle" style="color:{{$evenement->type->color}}"></i></span><a href="javascript:void(0)" class="popupEvent" data-url="{{ route('evenement_popup', $evenement) }}"><strong>{{$evenement->title}}</strong><a></td>
+        <td class="fc-list-event-title col-md-auto">{{$evenement->organisme->nom}}</td>
+        <td class="fc-list-event-title col-md-auto"><button type="button" class="btn btn-primary" onclick="exportICS({{$evenement->id}})"><i class="fas fa-external-link-alt"></i></button></td>
       </tr>
-    @foreach($evenements as $evenement)
-    @if($evenement->active == false)
-    <tr class="fc-list-event  fc-event fc-event-start fc-event-end fc-event-past">
-      <td class="fc-list-event-time" style="width: 150px">{{ date('d F Y', strtotime($evenement->start)) }}</td>
-      <td class="fc-list-event-title"><span class="fc-list-dot col-md-auto"><i class="fas fa-circle" style="color:{{$evenement->type->color}}"></i></span>{{$evenement->title}}</td>
-      <td class="fc-list-event-title">{{$evenement->type->nom}}</td>
-      <td class="fc-list-event-title" style="width: 300px">{{$evenement->organisme->nom}}</td>
-      <td class="fc-list-event-title"><button type="button" class="btn btn-primary" onclick="exportObligation()"><i class="fas fa-external-link-alt"></i></button></td>
-    </tr>
-    @endif
+      @endif
+      @endforeach
     @endforeach
+
     </tbody>
   </table>
 </div>
