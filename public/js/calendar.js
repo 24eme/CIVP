0e9bdc4f9b46e7876dfbdba7942f6bdd1e7c9650
Jb Lm <lemetayer.jb@gmail.com>
2021-04-01 18:21:04 +0200

    readaptation du js en fonction de la nouvelle librairie fullcalendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,77 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    headerToolbar: {
-      left:'prev,today,next',
-      center: 'title',
-      right: 'dayGridMonth,YearViewCustom'
-    },
-    locale: 'fr',
-    contentHeight:650,
-    eventDisplay:'block',
-    titleFormat:{year: 'numeric',month:'long'},
-    dayMaxEvents: true,
-    themeSystem: 'bootstrap',
-    displayEventEnd: true,
-    events:'evenement/list?output=json',
-    firstDay: 1,
-    datesSet:function(info){
-        Currentdate = calendar.getDate().toISOString();
-        DateTab = (Currentdate).split("T", 2);
-        Currentdate = DateTab[0];
-        var url = new URL(window.location);
-        url.searchParams.set('date', Currentdate);
-        if(window.location.href.includes('index')){
-          history.pushState(null,null, "index?date="+Currentdate);
-        };
-    },
-    fixedWeekCount: false,
-    buttonText:{ today:'Aujourd\'hui',month: 'Mois', week: 'Semaine', day: 'Jour'},
-    allDayText:'Journée',
-    bootstrapFontAwesome:{ month: 'fa-calendar-alt',week: 'fa-calendar-week' ,day:'fa-calendar-day' },
-    initialView: 'YearViewCustom',
-    eventClick: function(item){
-      $.get("evenement/"+item.event.id, function(response) {
-        $('#popupEvenement').html(response);
-        $('#popupEvenement').modal('show');
-      });
-    },
-    views: {
-      YearViewCustom: {
-      type: 'dayGrid',
-      buttonText: 'Année',
-      duration:{months:12},
-      dayMaxEventRows: 6
+(function($)
+{
+	$(document).ready(function() {
+    $('#calendar').fullCalendar({
+      header: {
+        left: 'prev title next',
+        center: '',
+        right: 'month,year'
+      },
+      locale: 'fr',
+      contentHeight: 650,
+      dayMaxEvents: true,
+      displayEventEnd: true,
+      events:'evenement/list?output=json',
+      firstDay: 1,
+      fixedWeekCount: false,
+      defaultView: 'year',
+      buttonText: {month: 'Mois', year: 'Année'},
+      eventClick: function(item){
+        $.get("evenement/"+item.id, function(response) {
+          $('#popupEvenement').html(response);
+          $('#popupEvenement').modal('show');
+        });
       }
-    }
+    });
   });
-  window.value = calendar;
-  calendar.render();
 
-  var removedEvents = new Array()
-  var removed = new Array()
-
-  $("input.filter-type").each(function (index, element) {
-      $(element).click(function(){
-        setCookie(this.id,this.checked)
-        var events = calendar.getEvents();
-        if (!this.checked) {
-          events.forEach(function(eve,i){
-            if (eve.extendedProps.type == element.value) { eve.remove();removedEvents.push(eve); }
-          })
-        } else{
-            removedEvents.forEach(function(removedEve,i){
-              if (removedEve.extendedProps.type == element.value) {
-                calendar.addEvent(removedEve);
-              } else{
-                if (removed.includes(removedEve) == false) {
-                  removed.push(removedEve)
-                }
-              }
-            })
-            removedEvents = removed
-        }
-      })
-    })
-
-});
+})(jQuery);
