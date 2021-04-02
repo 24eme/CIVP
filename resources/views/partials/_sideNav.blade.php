d803e5e489dfd0a2609c3032a300c3cc5ae4210e
Jb Lm <lemetayer.jb@gmail.com>
2021-04-02 18:50:20 +0200

    gestion des filtres par formulaire

--- resources/views/partials/_sideNav.blade.php
+++ resources/views/partials/_sideNav.blade.php
@@ -12,6 +12,8 @@
     @include('partials/_adminNav')
   @endif
 
+  <form action="{{ route('listEvenements') }}" method="GET" id="formFilters">
+
   <div class="row p-2 mt-3 FiltrerPar">
     <div class="col-md-12 pt-1">
       <h5>Filtrer par</h5>
@@ -26,7 +28,7 @@
     <div class="col-md-12 mb-0 p-0">
       @foreach($familles as $famille)
         <div class="custom-control custom-switch py-1">
-          <input type="checkbox" class="custom-control-input" id="famille{{$famille->id}}">
+          <input name="filters[familles][]" value="{{$famille->id}}" type="checkbox" class="custom-control-input" id="famille{{$famille->id}}">
           <label class="custom-control-label" for="famille{{$famille->id}}">{{$famille->nom}}</label>
         </div>
       @endforeach
@@ -41,7 +43,7 @@
     <div class="col-md-12 mb-0 p-0">
       @foreach($organismes as $organisme)
         <div class="custom-control custom-switch py-1">
-          <input type="checkbox" class="custom-control-input" id="organisme{{$organisme->id}}">
+          <input name="filters[organismes][]" value="{{$organisme->id}}" type="checkbox" class="custom-control-input" id="organisme{{$organisme->id}}">
           <label class="custom-control-label" for="organisme{{$organisme->id}}">{{$organisme->nom}} {{$organisme->couleur}}</label>
         </div>
       @endforeach
@@ -53,12 +55,16 @@
   </div>
 
   <div class="row ml-4">
-    <div class="tagcloud">
-      <a href="#" class="tag-cloud-link">CIVP</a>
-      <a href="#" class="tag-cloud-link">IGP</a>
-      <a href="#" class="tag-cloud-link">AVA</a>
-    </div>
+      @foreach($tags as $tag)
+      <div class="btn-group-toggle m-1" data-toggle="buttons">
+        <label class="btn btn-sm btn-outline-danger">
+          <input name="filters[tags][]" value="{{$tag->id}}" type="checkbox"> {{$tag->nom}}
+        </label>
+      </div>
+      @endforeach
   </div>
+
+  </form>
 
   <div class="row p-2 mt-3 FiltrerPar">
     <div class="col-md-12 pt-1">
