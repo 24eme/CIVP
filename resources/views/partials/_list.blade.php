b827f6ae2d15f5c2290cb42298f522c306452513
Jb Lm <lemetayer.jb@gmail.com>
2021-04-07 19:00:28 +0200

    optimisation de la vue liste

--- resources/views/partials/_list.blade.php
+++ resources/views/partials/_list.blade.php
@@ -2,23 +2,17 @@
 
 <table class="table table-hover">
     @foreach($evenements as $evenement)
-    @if(in_array($evenement->id, $done) == false)
-    <thead class="thead-light">
-      <tr>
-          <th colspan="3">{{ \Carbon\Carbon::parse($evenement->start)->translatedFormat('d F Y') }}</th>
-      </tr>
-    </thead>
-    @endif
-    @foreach($evenements as $subevenement)
-    @if($subevenement->start == $evenement->start && in_array($subevenement->id, $done) == False)
-    @php(array_push($done,$subevenement->id))
-      <tr>
-        <td class⁼"col-2"><small class="text-muted">{{ $subevenement->type->name }}</span> <a href="javascript:void(0)" class="popupEvent" data-url="{{ route('evenement_popup', $evenement) }}"><strong>{{$evenement->title}}</strong><a></td>
-        <td class⁼"col-1">@if($subevenement->end)<small class="text-muted">fin le</small>&nbsp;{{ \Carbon\Carbon::parse($evenement->end)->translatedFormat('d F Y') }} @endif</td>
-        <td class⁼"col-1">{{$evenement->strOrganismes()}}</td>
-        <td><button type="button" class="btn btn-primary" onclick="exportICS({{$evenement->id}})"><i class="fas fa-external-link-alt"></i></button></td>
+    @if(!in_array($evenement->start, $done))
+      <tr class="thead-light">
+          <th colspan="4">{{ \Carbon\Carbon::parse($evenement->start)->translatedFormat('d F Y') }}</th>
       </tr>
     @endif
-    @endforeach
+    @php(array_push($done, $evenement->start))
+      <tr class="row m-0">
+        <td class="col-1">@if($evenement->end)<small class="text-muted"><i class="fas fa-chevron-right small"></i>&nbsp;{{ \Carbon\Carbon::parse($evenement->end)->translatedFormat('d F Y') }}</small>@endif</td>
+        <td class="col-5"><a href="javascript:void(0)" class="popupEvent" data-url="{{ route('evenement_popup', $evenement) }}"><strong>{{$evenement->title}}</strong><a></td>
+        <td class="col-5">{!! $evenement->htmlOrganismesList() !!}</td>
+        <td class="col-1"><button type="button" class="btn btn-primary" onclick="exportICS({{$evenement->id}})"><i class="fas fa-external-link-alt"></i></button></td>
+      </tr>
     @endforeach
 </table>
