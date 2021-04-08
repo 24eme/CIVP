385465aa5d2bebd2bb13ade910baded99a9e6420
Jb Lm <lemetayer.jb@gmail.com>
2021-04-08 19:07:39 +0200

    information de l'utilisateur loggue + lien de deconnexion, on deplace egalement le btn d'export ical

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,19 @@
 @extends('layout')
 @section('content')
 @include('partials/_sideNav')
-<div id="main" class="main">
+<div id="main" class="main col-10">
   <nav class="mt-4">
     <div class="col-md-auto float-right">
-      <button style="background-color:#E8E8E8;border-color:#E8E8E8" type="button" class="btn btn-warning" data-dismiss="modal"><a href="export">Exporter</a></button>
+      @if($user)
+      <div class="btn-group">
+        <button class="btn btn-primary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-user"></i>&nbsp;{{ $user->name }}
+        </button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="{{ route('logout') }}">Déconnexion</a>
+        </div>
+      </div>
+      @endif
     </div>
     <div class="nav nav-tabs" id="nav-tab" role="tablist">
       <a class="nav-link active" id="nav-calendrier-tab" data-toggle="tab" href="#nav-calendrier" role="tab" aria-controls="nav-calendrier" aria-selected="true">Calendrier</a>
@@ -19,6 +28,7 @@
     <div class="tab-pane fade" id="nav-liste" role="tabpanel" aria-labelledby="nav-liste-tab">
       @include('partials/_list')
     </div>
+    <a type="button" class="btn btn-primary float-right my-3 mr-2" href="{{ route('export') }}">Export Ical</a></a>
   </div>
 </div>
 
