04dbe6074fcdd8a96d4c2a17fc1be21c34d0f912
Jb Lm <lemetayer.jb@gmail.com>
2021-04-08 19:08:54 +0200

    js pour le btn collapse bootstrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,6 +8,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment-with-locales.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="/fullcalendar-year-view/dist/fullcalendar.js"></script>
 
