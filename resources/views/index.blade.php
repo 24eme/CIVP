1f67de4f7c188ac2af4d18ac004e87b47a3f4a88
Jb Lm <lemetayer.jb@gmail.com>
2021-04-12 19:10:50 +0200

    adaptation css pour l'admin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,11 +19,11 @@
       <a class="nav-link active" id="nav-calendrier-tab" data-toggle="tab" href="#nav-calendrier" role="tab" aria-controls="nav-calendrier" aria-selected="true">Calendrier</a>
       <a class="nav-link" id="nav-liste-tab" data-toggle="tab" href="#nav-liste" role="tab" aria-controls="nav-liste" aria-selected="false">Liste</a>
     </div>
-
   </nav>
+
   <div class="tab-content mt-4" id="nav-tabContent">
     <div class="tab-pane fade show active" id="nav-calendrier" role="tabpanel" aria-labelledby="nav-calendrier-tab">
-      <div id='calendar'></div>
+      <div id='calendar' class="mainContent"></div>
     </div>
     <div class="tab-pane fade" id="nav-liste" role="tabpanel" aria-labelledby="nav-liste-tab">
       @include('partials/_list')
