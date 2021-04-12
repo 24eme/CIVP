d3eed6a3011508dbb30bae1dbfa563710dd29c5e
Jb Lm <lemetayer.jb@gmail.com>
2021-04-12 19:03:30 +0200

    mise a jour des routes pour l'admin

--- routes/web.php
+++ routes/web.php
@@ -23,31 +23,25 @@ Route::prefix('admin')->group(function () {
   Route::get('/authenticated', [AdminController::class,'authenticated'])->name('authenticated');
   Route::get('/logout', [AdminController::class,'logout'])->name('logout');
 
-  Route::post('/importCSV', [CalendarController::class,'importCSV'])->name('importCSV');
-
-  Route::get('evenements', [EvenementController::class,'index'])->name('evenements');
   Route::get('evenement/create', [EvenementController::class,'create'])->name('evenement_create');
   Route::post('evenement/create', [EvenementController::class,'store']);
   Route::get('evenement/edit/{evenement}', [EvenementController::class,'edit'])->name('evenement_edit');
   Route::post('evenement/edit/{evenement}', [EvenementController::class,'update']);
-  Route::get('evenement/export/{id}', [EvenementController::class,'exportEvenement'])->name('evenement_export');
 
   Route::get('organismes', [OrganismeController::class,'index'])->name('organismes');
   Route::get('organisme/create', [OrganismeController::class,'create'])->name('organisme_create');
   Route::post('organisme/create', [OrganismeController::class,'store']);
   Route::get('organisme/edit/{organisme}', [OrganismeController::class,'edit'])->name('organisme_edit');
   Route::post('organisme/edit/{organisme}', [OrganismeController::class,'update']);
-
+  
   Route::get('types', [TypeController::class,'index'])->name('types');
-  Route::get('type/create', [TypeController::class,'create'])->name('type_create');
-  Route::post('type/create', [TypeController::class,'store']);
   Route::get('type/edit/{type}', [TypeController::class,'edit'])->name('type_edit');
   Route::post('type/edit/{type}', [TypeController::class,'update']);
 
   Route::get('familles', [FamilleController::class,'index'])->name('familles');
-  Route::get('famille/create', [FamilleController::class,'create'])->name('famille_create');
-  Route::post('famille/create', [FamilleController::class,'store']);
   Route::get('famille/edit/{famille}', [FamilleController::class,'edit'])->name('famille_edit');
   Route::post('famille/edit/{famille}', [FamilleController::class,'update']);
+
+  Route::post('/importCSV', [CalendarController::class,'importCSV'])->name('importCSV');
 
 });
