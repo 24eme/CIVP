e715333d1ef606fbfb3bd23247583fa1c4c16e47
Jb Lm <lemetayer.jb@gmail.com>
2021-04-29 19:27:15 +0200

    footer like vindeprovence.com

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,27 +1,26 @@
-<footer id="footer" class="bg-dark text-center text-white">
-  <div class="container p-4">
-    <section class="mb-4">
-      <p>Les Vins de Provence sont fiers de vous présenter un Calendrier destiné aux différents familles viticules mais également aux particuliers amateurs de Vins</p>
-    </section>
-    <section class="">
-      <div class="row">
-        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-          <h5 class="text-uppercase">CIVP</h5>
-        </div>
-        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-          <h5 class="text-uppercase">A PROPOS</h5>
-        </div>
-        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-          <h5 class="text-uppercase">CONTACT</h5>
-        </div>
-        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-          <h5 class="text-uppercase">MENTIONS LEGALES</h5>
-        </div>
-      </div>
-    </section>
+<footer id="footer" class="text-center">
+  <div class="py-4 footer-icon">
+    <a href="/"><img src="{{asset('images/logos/logo-C-footer.png')}}" width=70  alt=""></a>
   </div>
-  <div class="text-center p-3 bg-dark"  >
-    © 2021 Vins de Provence:
-    <a class="text-white" href="https://www.vinsdeprovence.com/">vinsdeprovence.com</a>
+  <div class="footer-links container">
+    <ul class="footer-nav" role="menubar">
+      <li class="nav-item" id="nav-item-civp" role="menuitem">
+        <a class="nav-link" id="nav-link-civp" href="#">CIVP</a>
+      </li>
+      <li class="nav-item" id="nav-item-contact" role="menuitem">
+        <a class="nav-link" id="nav-link-contact" href="#">Contact</a>
+      </li>
+      <li class="nav-item" id="nav-item-telechargement" role="menuitem">
+        <a class="nav-link" id="nav-link-telechargement" href="#">Téléchargement</a>
+      </li>
+      <li class="nav-item" id="nav-item-mentions-legales" role="menuitem">
+        <a class="nav-link" id="nav-link-mentions-legales" href="#">Mentions légales</a>
+      </li>
+    </ul>
+  </div>
+  <div class="footer-disclaimer">
+        <div class="container">
+            <a href="https://www.vinsdeprovence.com" class="" target="_blank">www.vinsdeprovence.com</a>
+        </div>
   </div>
 </footer>
