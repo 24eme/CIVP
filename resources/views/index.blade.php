31ffe58f45c549b5906c502bf5691cbcf7019a03
Mame <mngom@24eme.fr>
2021-05-04 12:21:04 +0200

    Ajout des Organismes au dessus du Calendrier

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,6 +29,20 @@
       <a class="nav-link" id="nav-liste-tab" data-toggle="tab" href="#nav-liste" role="tab" aria-controls="nav-liste" aria-selected="false">Liste</a>
     </div>
   </nav>
+
+  <div class="row ml-0">
+    <div><h5 class="col-md-12 mt-2 px-0 py-1">Organismes</h5></div>
+  </div>
+  <div class="row ml-4">
+      @foreach($organismes as $organisme)
+      <div class="col-md-3 mb-0 p-0">
+        <div class="custom-control custom-switch py-1">
+          <input name="filters[organismes][]" value="{{$organisme->id}}" type="checkbox" class="custom-control-input custom-control-input-{{$organisme->slug}}" id="organisme{{$organisme->id}}">
+          <label class="custom-control-label custom-control-label-{{$organisme->slug}}" for="organisme{{$organisme->id}}"><img src="/images/logos/organismes/{{$organisme->logo}}" class="" height="20px" /> {{$organisme->nom}}</label>
+        </div>
+            </div>
+      @endforeach
+  </div>
   <div class="tab-content mt-4" id="nav-tabContent">
     <a type="button" class="btn btn-primary float-right my-3 mx-2" href="{{ route('export') }}"><i class="far fa-calendar-alt"></i> Exporter</a>
     <div class="tab-pane fade show active" id="nav-calendrier" role="tabpanel" aria-labelledby="nav-calendrier-tab">
