e8ecea05f60f4756de8aa7ce7fa70d1e24cb6263
Mame <mngom@24eme.fr>
2021-05-04 13:17:29 +0200

    Remplace chevron > par au dans Vue Liste

--- resources/views/partials/_list.blade.php
+++ resources/views/partials/_list.blade.php
@@ -9,7 +9,7 @@
     @endif
     @php(array_push($done, $evenement->start))
       <tr class="row m-0">
-        <td class="col-1">@if($evenement->end)<small class="text-muted"><i class="fas fa-chevron-right small"></i>&nbsp;{{ \Carbon\Carbon::parse($evenement->end)->translatedFormat('d M Y') }}</small>@endif</td>
+        <td class="col-1">@if($evenement->end)<small class="text-muted">au {{ \Carbon\Carbon::parse($evenement->end)->translatedFormat('d M Y') }}</small>@endif</td>
         <td class="col-5">@if (!$evenement->active) <i class="fas fa-circle" style="color: red"></i> @endif<a href="javascript:void(0)" class="popupEvent" data-url="{{ route('evenement_popup', $evenement) }}"><strong>{{$evenement->title}}</strong><a></td>
         <td class="col-5">{!! $evenement->htmlOrganismesList() !!}</td>
         <td class="col-1">@if ($user) <a href="{{ route('evenement_edit', $evenement) }}"><i class="far fa-edit">&nbsp;</i></a> @endif</td>
