d6605fe3543b145ad75c772f905d965935761351
Jb Lm <lemetayer.jb@gmail.com>
2021-05-21 19:05:07 +0200

    amelioration inte popup

--- resources/views/partials/popup.blade.php
+++ resources/views/partials/popup.blade.php
@@ -35,48 +35,63 @@
 
     <div class="modal-body">
       <div class="row">
-        <div class="col-7 row">
-          <div class="col-12">
-            <label class="popup-label" for="">Famille(s) : </label>{{ $evenement->strFamilles() }}
-          </div>
-          <div class="col-12">
-            <p>{!! $evenement->description !!}</p>
-          </div>
+        <div class="col-2">
+          <label class="popup-label" for="">Famille(s)</label>
+        </div>
+        <div class="col-10 row">
+          {{ $evenement->strFamilles() }}
+        </div>
+      </div>
+
+      <div class="row">
+
+        <div class="col-7">
+            <p class="mt-4 mb-0">
+              <strong>Information</strong>
+            </p>
+            {!! $evenement->description !!}
         </div>
 
-        <div class="col-5 row d-inline-block">
+        <div class="col-5">
           @foreach($evenement->organismes as $organisme)
-          <div class="organisme-card my-1 p-1 row d-inline-block w-100 ml-4">
+          <div class="organisme-card my-1 p-1 ml-4">
             <div class="col-12 pb-2">
               <img src="/images/logos/organismes/{{$organisme->logo}}" class="" height="20px" /> <i class="fas fa-circle" style="color: {{ $organisme->couleur }}"></i>&nbsp;<strong>{{$organisme->nom}}</strong>
             </div>
             <div class="col-12 pb-2">
               {{$organisme->adresse}}<br />{{$organisme->code_postal}} {{$organisme->ville}}
             </div>
+            @if($organisme->contact)
             <div class="col-12 pb-1">
               <strong>{{$organisme->contact}}</strong>
             </div>
+            @endif
+            @if($organisme->telephone)
             <div class="col-12">
               <i class="fas fa-phone-alt"></i>&nbsp;{{$organisme->telephone}}
             </div>
+            @endif
+            @if($organisme->email)
             <div class="col-12">
               <i class="far fa-envelope"></i>&nbsp;{{$organisme->email}}
             </div>
+            @endif
           </div>
           @endforeach
         </div>
+
       </div>
 
       <div class="row">
-        <div class="col-1">
+        <div class="col-2 pt-2">
           <label class="popup-label" for="">Mots-Clés</label>
         </div>
-        <div class="col-11 row">
+        <div class="col-10 row">
           @foreach($evenement->tags as $tag)
-          <div class="btn-group-toggle m-1" data-toggle="buttons">
-            <label class="btn btn-sm btn-outline-danger">
+          <div class="m-1">
+            <a href="{{ route('index') }}?filters[tags][]={{$tag->id}}" class="btn btn-sm btn-outline-danger">
               {{$tag->nom}}
-            </label>
+            </a>
           </div>
           @endforeach
         </div>
@@ -90,10 +105,10 @@
       </div>
 @endif
     </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" ><a href="evenement/export/{{$evenement->id}}"><i class="far fa-calendar-check"></i> Exporter</a></button>
+    <div class="modal-footer d-inline-block">
+      <a href="evenement/export/{{$evenement->id}}" class="btn btn-primary float-left"><i class="far fa-calendar-alt"></i> Exporter</a>
 @if($evenement->liendeclaration)
-      <button type="button" class="btn btn-primary"><a href="{{$evenement->liendeclaration}}"><i class="fas fa-external-link-alt"></i> Accéder à la déclaration</a></button>
+      <a href="{{$evenement->liendeclaration}}" class="btn btn-outline-danger float-right"><i class="fas fa-external-link-alt"></i> Accéder à la déclaration</a>
 @endif
     </div>
   </div>
