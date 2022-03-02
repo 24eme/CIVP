f7134c477140f128fbfa260b35175c184667f2b9
Jb Lm <lemetayer.jb@gmail.com>
2022-03-02 11:11:11 +0100

    on remplace le logo C par le P

--- resources/views/admin/authenticated.blade.php
+++ resources/views/admin/authenticated.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="form-signin mt-5">
-  <img class="mb-4" src="{{asset('images/logos/logo-C.png')}}" alt="" width="72" height="72">
+  <img class="mb-4" src="{{asset('images/logos/logo-P.png')}}" alt="" width="72" height="72">
   <h1 class="h3 mb-3 font-weight-normal">Administration</h1>
   <p>Bonjour <strong>{{ $user->name }}</strong>,</p>
   <p>Vous êtes actuellement connecté en tant qu'admin.</p>
