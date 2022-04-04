538826cbe07704ea4d7229c10e87886baa67db15
Jb Lm <lemetayer.jb@gmail.com>
2022-04-04 17:22:41 +0200

    les filtres sont memorises pour 12h

--- public/js/global.js
+++ public/js/global.js
@@ -123,7 +123,7 @@ function moveToMonth(date){
 // IMPORTANT POUR COOKIE
 function setCookie(name, value) {
     var date = new Date();
-    date.setTime(date.getTime() + (365*24*60*60*1000));
+    date.setTime(date.getTime() + (12*60*60*1000));
     expires = "; expires=" + date.toUTCString();
     document.cookie = name + "=" + (value)  + expires + "; path=/";
 }
