0c68bdd50ee1587ff59c30103c467105223e5ab0
Jb Lm <lemetayer.jb@gmail.com>
2022-05-16 17:58:06 +0200

    target blank url oranisme

--- resources/views/partials/popup.blade.php
+++ resources/views/partials/popup.blade.php
@@ -83,7 +83,7 @@
             @endif
             @if($organisme->site)
             <div class="col-12">
-              <i class="fas fa-globe"></i>&nbsp;<a href="{{$organisme->site}}">{{$organisme->site}}</a>
+              <i class="fas fa-globe"></i>&nbsp;<a href="{{$organisme->site}}" target="_blank">{{$organisme->site}}</a>
             </div>
             @endif
           </div>
